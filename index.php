875771da54dffc69a1aaa9a37923aa2ba0139515
i-like-dev <136171623+i-like-dev@users.noreply.github.com>
2023-10-25 20:58:34 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?
+    echo "方法一"
+    ?>
+    <?php
+    echo "方法二"
+    ?>
+    <SCRIPT LANGUAGE="php">
+        echo "方法三";
+        </script>
+</body>
+</html>
